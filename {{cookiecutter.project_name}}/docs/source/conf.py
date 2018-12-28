51b51b54514bd5a73d3093b05fd1133882cd2c31
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-12-27 17:35:42 -0700

    Update cookiecutter template var

--- {{cookiecutter.project_name}}/docs/source/conf.py
+++ {{cookiecutter.project_name}}/docs/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'{{ cookiecutter.project_name }}'
-copyright = u'{{ cookiecutter.year }}, {{ cookiecutter.full_name }}'
+copyright = u'{{ cookiecutter.year }} onwards, {{ cookiecutter.github_username }}'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
