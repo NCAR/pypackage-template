ba9b825fdc74cd66e0829118ea02e6059f84f72f
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-04-10 16:27:23 -0600

    Switch from versioneer to setuptools-scm

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 """Top-level module for {{ cookiecutter.project_slug }}."""
-from ._version import get_versions
+from pkg_resources import DistributionNotFound, get_distribution
 
-__version__ = get_versions()["version"]
-del get_versions
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
