1673a5d19ebdf440dc9fb4187f938d27c773be0d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-04-10 23:39:22 -0600

    Fix bugs

--- {{cookiecutter.project_name}}/ci/run-linter.sh
+++ {{cookiecutter.project_name}}/ci/run-linter.sh
@@ -8,7 +8,7 @@ echo "Code Styling with (black, flake8, isort)"
 source activate ${ENV_NAME}
 
 echo "[flake8]"
-flake8
+flake8 {{ cookiecutter.project_name }}
 
 echo "[black]"
 black --check --line-length=100 -S .
